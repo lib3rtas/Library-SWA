34554a567b419b983cff9a5f2f3df2ec0affe96a
Florian Hammer <florian.hammer.1996@web.de>
2020-12-10 13:23:27 +0100

    Initial project commit

---
+++ app/src/test/java/ch/fhnw/swa/library/app/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.swa.library.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web/src/test/java/ch/fhnw/swa/library/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.swa.library.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
