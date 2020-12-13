d38f5f5fad40563b1aa89211a53f2fd26a610893
Florian Hammer <florian.hammer@students.fhnw.ch>
2020-12-13 14:03:49 +0100

    fully implemented Book model class

--- domain/src/main/java/ch/fhnw/swa/library/book/Book.java
+++ domain/src/main/java/ch/fhnw/swa/library/book/Book.java
@@ -1,11 +1,29 @@
 package ch.fhnw.swa.library.book;
 
-import lombok.Value;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.FieldDefaults;
 
 import java.util.UUID;
 
-@Value
+// Can't just use @Value, because thymeleaf requires an non-immutable object
+@Getter
+@Setter
+@FieldDefaults(makeFinal = false, level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Book {
     UUID    id;
     String  title;
+    String  description;
+
+    // Cstor for thymeleaf
+    public Book(){
+        this.id = UUID.randomUUID();
+    }
 }
