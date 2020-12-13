6c103120c33c9c9d529af4bc75400af1906d3e49
Florian Hammer <florian.hammer@students.fhnw.ch>
2020-12-13 14:12:17 +0100

    simple sanity check test to see if all controllers are loaded into the application

--- app/src/test/java/ch/fhnw/swa/library/LibraryApplicationTests.java
+++ app/src/test/java/ch/fhnw/swa/library/LibraryApplicationTests.java
@@ -1,13 +1,20 @@
 package ch.fhnw.swa.library;
 
+import ch.fhnw.swa.library.web.BookController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LibraryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private BookController bookController;
 
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(bookController).isNotNull();
+	}
 }
