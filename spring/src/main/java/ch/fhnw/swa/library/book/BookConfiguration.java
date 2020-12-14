3f0a0780ddc210050b4455d9c6932a819b1f9435
Florian Hammer <florian.hammer@students.fhnw.ch>
2020-12-14 02:12:48 +0100

    enhanced spring config, so that it can be switched between different repository implementation by changing a single string in the config file

--- spring/src/main/java/ch/fhnw/swa/library/book/BookConfiguration.java
+++ spring/src/main/java/ch/fhnw/swa/library/book/BookConfiguration.java
@@ -1,16 +1,47 @@
 package ch.fhnw.swa.library.book;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BookConfiguration {
 	@Bean
-	IBookService bookService(IBookRepository bookRepository){
+	IBookService bookService(@Qualifier("h2") IBookRepository bookRepository){
 		return new BookService(bookRepository);
 	}
+
+	@Bean("h2")
+	IBookRepository h2BookRepository(JdbcTemplate jdbcTemplate, IBookFactory bookFactory){
+		return new H2BookRepository(jdbcTemplate, bookFactory);
+	}
+
+	@Bean("list")
+	IBookRepository listBookRepository(IBookFactory bookFactory){
+		return new ListBookRepository(bookFactory);
+	}
+
 	@Bean
-	IBookRepository bookRepository(){
-		return new ListBookRepository();
+	JdbcTemplate jdbcTemplate(){
+		return new JdbcTemplate(h2DataSource());
+	}
+
+	@Bean
+	DataSource h2DataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.hibernate.dialect.H2Dialect");
+		dataSource.setUrl("jdbc:h2:file:~/data");
+		dataSource.setUsername("root");
+		dataSource.setPassword("password");
+		return dataSource;
+	}
+
+	@Bean
+	IBookFactory bookFactory(){
+		return new BookFactory();
 	}
 }
